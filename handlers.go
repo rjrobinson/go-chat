5b23f1ae4a3078cb64746dddfb71da86f60d6885
RJ Robinson <Robert_Robinson3@cable.comcast.com>
2016-03-13 11:55:56 -0400

    moved handlers to own file for clarity'

---
+++ handlers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
